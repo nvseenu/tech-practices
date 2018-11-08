4c1aee16870d88c198ba6293eeaeeb4fb3f563bf
nvseenu <nvseenu@gmail.com>
2018-11-08 15:36:12 +0530

    Initial commit to competetitive problems

---
+++ competitive-problems-py/maximum_difference.py
@@ -0,0 +1,14 @@
+def maximum_difference(a):
+    pass
+
+def distinct_pairs(a):
+    pass
+
+def anagram_difference(a):
+    pass
+
+def degree_of_array():
+    pass
+
+def consecutive_sum():
+    pass                
