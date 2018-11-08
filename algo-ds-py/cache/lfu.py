00f34aad07d90ed9d49cdb70b50d98ae41231d19
nvseenu <nvseenu@gmail.com>
2018-11-08 15:35:24 +0530

    Imlemented LFU

---
+++ algo-ds-py/cache/lfu.py
@@ -0,0 +1,38 @@
+from priority_queue import PriorityQueue
+from heap import Entry
+
+class LFU:
+    """
+        Least Frequently Used cache. It keeps elements that are frequently used,
+        and evicts less frequently used ones when the cache is full.
+    """
+
+    def __init__(self, capacity):
+        self._capacity = capacity
+        self._priority_queue = PriorityQueue(capacity)
+
+
+    def get(self, key):
+        entry = self._priority_queue.get_entry(key)
+        if not entry:
+            return None
+
+        self._priority_queue.set_priority(key, entry._priority + 1)
+        return entry._key
+    
+
+    def put(self, key):
+        self.evict_if_needed()
+        self._priority_queue.enqueue(key, 0)
+        
+
+
+    def evict_if_needed(self):
+        if len(self._priority_queue) == self._capacity:  
+            e = self._priority_queue.dequeue()
+
+    
+    def __len__(self):
+        return len(self._priority_queue)        
+
+
---
+++ algo-ds-py/tests/cache/test_lfu.py
@@ -0,0 +1,58 @@
+from cache.lfu import LFU
+import unittest
+
+
+class TestLFU(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+    
+    def tearDown(self):
+        pass
+
+
+    def test_contains(self):
+        """It tests if a cache keeps all elements stored into. Assume total elements are less than
+        total capacity. hence there is no eviction.
+        """
+
+        lfu = LFU(4)  
+        for i in range(1,5):
+            lfu.put(i)
+
+        for i in range(1,5):
+            self.assertEqual(i, lfu.get(i), "Cache does not contain a element that was stored before")    
+
+    def test_eviction(self):
+        """It tests if right element has been evicted out from the cache especially
+        when the cache is full and we try to store a new element.
+        """
+        lfu = LFU(4)        
+
+        lfu.put(1)
+        lfu.put(2)
+        lfu.put(3)
+        lfu.put(4)
+
+        lfu.get(1)
+        lfu.get(3)
+
+        # Inserting 5 needs a room but lfu is full. 
+        # since 2 is less frequently used item , it should be evicted
+        lfu.put(5)
+        self.assertEqual(4, len(lfu), "LFU length is not correct")
+        self.assertIsNone(lfu.get(2), "Eviction is not happening properly")
+
+        lfu.get(5)
+
+        # Inserting 6 needs a room but lfu is full. 
+        # since 4 is less frequently used item , it should be evicted
+        lfu.put(6)
+        self.assertEqual(4, len(lfu), "LFU length is not correct")
+        self.assertIsNone(lfu.get(4), "Eviction is not happening properly")
+
+
+
+        
