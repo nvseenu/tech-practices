3fef928d45deb329859152010e0c230fbdf31a16
nvseenu <nvseenu@gmail.com>
2018-12-13 14:00:33 +0530

    Implemented array rotation

--- geek-for-geek/geek_for_geek/array.py
+++ geek-for-geek/geek_for_geek/array.py
@@ -16,3 +16,61 @@ def find_largest_sum_in_contiguous_subarray(arr):
 
     
     return max_sum           
+
+
+def left_rotate(arr, n):
+    """
+    Rotate elements in the array by given position
+
+    Args:
+    arr(array) -> Array of elements
+    n(int) -> Number of positions to rotate
+
+    Time complexity: O(n^ number of rotations) 
+    """
+    
+    for _ in range(n):
+        _one_left_rotation(arr)
+
+
+
+def _one_left_rotation(arr):
+    t = arr[0]    
+    n = len(arr)
+    for i in range(n-1):
+        arr[i] = arr[i+1]        
+    
+    arr[n-1] = t
+
+
+def left_rotate1(arr, d):
+    """
+    Rotate elements in the array by given position
+
+    Args:
+    arr(array) -> Array of elements
+    d(int) -> Number of positions to rotate
+
+    Time complexity: O(n) 
+    Space complexity: O(d)
+    """    
+
+    # Create a temp array and copy first d elements
+    tarr = arr[:d]
+    n = len(arr)
+
+    # Move remaining elemnts to d position front
+    for i in range(d, n):
+        arr[i-d] = arr[i]
+
+    index = n-d    
+    for a in tarr:
+        arr[index] = a
+        index += 1       
+
+
+
+
+    
+        
+                
--- geek-for-geek/tests/test_array.py
+++ geek-for-geek/tests/test_array.py
@@ -5,12 +5,13 @@ class TestArray(unittest.TestCase):
 
     def setUp(self):
         pass
+        
 
     def tearDown(self):
         pass
 
-    def test_find_largest_sum_in_contiguous_subarray(self):
 
+    def test_find_largest_sum_in_contiguous_subarray(self):
         arr = [4, -3, -1, 7, 1, -3]
         sum = array.find_largest_sum_in_contiguous_subarray(arr)
         self.assertEqual(8, sum)
@@ -24,6 +25,33 @@ class TestArray(unittest.TestCase):
         arr = [-4, -3, -1, -7, -1, -3]
         sum = array.find_largest_sum_in_contiguous_subarray(arr)
         self.assertEqual(0, sum)
+
+
+    def test_left_rotate(self):
+        input = [1,2,3,4,5]        
+        expected = [4,5,1,2,3]
+
+        # Since the left rotation is a mutable operation,
+        # We need to take a copy before sending it.
+        arr = input[:]
+        array.left_rotate(arr, 3)
+        self.assertEqual(expected, arr)
+
+
+    def test_left_rotate1(self):
+        input = [1,2,3,4,5]        
+        expected = [4,5,1,2,3]
+
+        # Since the left rotation is a mutable operation,
+        # We need to take a copy before sending it.
+        arr = input[:]
+        array.left_rotate1(arr, 3)
+        self.assertEqual(expected, arr)    
+
+
+
+
+
 
         
 
